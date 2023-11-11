5460842935f766f093400b92f7a00ea5bf23c0fe
Subhadeep Roy <subha9.5roy350@gmail.com>
2023-11-11 16:23:47 +0530

    okk

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ import Meme from './Components/Meme'
 function App() {
   return (
     <>
-      <div className='p-12 flex flex-col gap-4 items-center justify-center sm:pt-4 sm:px-4'>
+      <div className='p-12 flex flex-col gap-4 items-center justify-center sm:pt-4 sm:px-4 pb-8'>
         <Meme/>
       </div>
-
+      <footer className="text-gray-300 text-center mb-2 absolute bottom-0 w-full">Made By <a href="https://github.com/subhadeeproy3902" className="text-sky-300">Subhadeep Roy</a></footer>
     </>
   )
 }
--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -58,6 +58,7 @@ const Meme = () => {
   };
 
   const handleButtonClick = () => {
+    setIsClicked(false)
     fetchMeme();
   };
 
